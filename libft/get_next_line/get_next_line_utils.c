af7ffba7fa28e3e535d5c06a72843a101163a0a3
htran312 <huongcake312@gmail.com>
2024-10-11 21:15:15 +0300

    nothing yet

---
+++ libft/get_next_line/get_next_line_utils.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htran-th <htran-th@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 16:12:05 by htran-th          #+#    #+#             */
+/*   Updated: 2024/09/30 20:25:42 by htran-th         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+char	*ft_strchr_gnl(const char *str, int c)
+{
+	int		i;
+	char	ch;
+
+	ch = c;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == ch)
+			return ((char *)&str[i]);
+		i++;
+	}
+	if (str[i] == ch)
+		return ((char *) &str[i]);
+	else
+		return (NULL);
+}
+
+void	*ft_memcpy_gnl(void *to, const void *from, size_t n)
+{
+	size_t		i;
+	char		*destination;
+	const char	*source;
+
+	destination = (char *)to;
+	source = (const char *)from;
+	if (!to && !source)
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		destination[i] = source[i];
+		i++;
+	}
+	return (to);
+}
+
+size_t	ft_strlen_gnl(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strdup_gnl(char *s1)
+{
+	size_t	i;
+	size_t	len;
+	char	*ptr;
+
+	len = ft_strlen_gnl(s1);
+	ptr = (char *) malloc ((len +1) * sizeof(char));
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+char	*ft_strjoin_gnl(char *s1, char *s2)
+{
+	char	*res;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	if (!s1 && !s2)
+		return (ft_strdup_gnl(""));
+	if (!s1)
+		return (ft_strdup_gnl(s2));
+	else if (!s2)
+		return (ft_strdup_gnl(s1));
+	s1_len = ft_strlen_gnl(s1);
+	s2_len = ft_strlen_gnl(s2);
+	res = malloc ((s1_len + s2_len +1) * (sizeof(char)));
+	if (!res)
+		return (NULL);
+	ft_memcpy_gnl(res, s1, s1_len);
+	ft_memcpy_gnl((res + s1_len), s2, s2_len);
+	res[s1_len + s2_len] = '\0';
+	free(s1);
+	return (res);
+}
---
+++ libft/libft.h
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htran-th <htran-th@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/23 19:18:14 by htran-th          #+#    #+#             */
+/*   Updated: 2024/10/03 16:03:13 by htran-th         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include <stddef.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+// ft_printf
+
+int				ft_putchar(char c);
+int				ft_putstr(char *str);
+int				ft_putaddress(unsigned long long int n);
+int				ft_putnbr(int n);
+unsigned int	ft_putunbr(unsigned int n, unsigned int base);
+unsigned int	ft_putxnbr_uppercase(unsigned int n, unsigned int base);
+int				ft_printf(const char *format, ...);
+
+// get_next_line
+
+char			*ft_strchr_gnl(const char *str, int c);
+ssize_t			read(int fildes, void *buf, size_t nbyte);
+char			*ft_strjoin_gnl(char *s1, char *s2);
+char			*ft_strdup_gnl(char *s1);
+size_t			ft_strlen_gnl(char *str);
+void			*ft_memcpy_gnl(void *to, const void *from, size_t n);
+char			*get_next_line(int fd);
+
+// libft
+
+int				ft_isalpha(int c);
+int				ft_isdigit(int c);
+int				ft_isalnum(int c);
+int				ft_isascii(int c);
+int				ft_isprint(int c);
+size_t			ft_strlen(const	char *str);
+void			*ft_memset(void *str, int c, size_t len);
+void			ft_bzero(void *str, size_t n);
+void			*ft_memcpy(void *to, const void *from, size_t n);
+void			*ft_memmove(void *to, const void *from, size_t n);
+size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t			ft_strlcat(char *dst, const char *src, size_t dstsize);
+int				ft_toupper(int c);
+int				ft_tolower(int c);
+char			*ft_strchr(const char *str, int c);
+char			*ft_strrchr(const char *str, int c);
+int				ft_strncmp(const char *s1, const char *s2, size_t n);
+void			*ft_memchr(const void *s, int c, size_t n);
+int				ft_memcmp(const void *s1, const void *s2, size_t n);
+char			*ft_strnstr(const char *haystack, const char *needle,
+					size_t len);
+int				ft_atoi(const char *str);
+void			*ft_calloc(size_t count, size_t size);
+char			*ft_strdup(const char *s1);
+char			*ft_substr(char const *s, unsigned int start, size_t len);
+char			*ft_strjoin(const char *s1, const char *s2);
+char			*ft_strtrim(char const *s1, char const *set);
+char			**ft_split(char const *s, char c);
+char			*ft_itoa(int n);
+char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void			ft_striteri(char *s, void (*f)(unsigned int, char*));
+void			ft_putchar_fd(char c, int fd);
+void			ft_putstr_fd(char *s, int fd);
+void			ft_putendl_fd(char *s, int fd);
+void			ft_putnbr_fd(int n, int fd);
+
+#endif
