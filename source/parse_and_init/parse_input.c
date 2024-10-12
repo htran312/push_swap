f85d6238d73a22c6e579ff3fedea7e459545981f
htran312 <huongcake312@gmail.com>
2024-10-12 20:56:09 +0300

    ?

--- source/parse_and_init/parse_input.c
+++ source/parse_and_init/parse_input.c
@@ -6,7 +6,7 @@
 /*   By: htran-th <htran-th@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 17:32:11 by htran-th          #+#    #+#             */
-/*   Updated: 2024/10/12 18:50:18 by htran-th         ###   ########.fr       */
+/*   Updated: 2024/10/12 20:09:28 by htran-th         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,11 @@ int parse_input(int argc, char **argv, t_stack *a)
         {
             //split the string and process each number
             temp = split_and_append(argv[i], temp);
-            if (!temp)
-                return (1);
         }
-        else //argv[i] is a normal int
+        else //argv[i] is not a string
         {
-            //atoi that int to temp
             //check if temp is valid
         }
         //either if argv[i] was a string or an int, join it to the array
-        array = ft_strjoin_gnl(array, temp);
     }
 }
